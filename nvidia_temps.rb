d7c7254e7c63b5d7be35ecb6774ac9fe6778919d
Chris Habgood <chabgood@gmail.com>
2020-02-02 22:18:45 -0600

    same as fah_stats program

--- nvidia_temps.rb
+++ nvidia_temps.rb
@@ -1,12 +1,9 @@
 #!/home/chabgood/.rbenv/shims/ruby
+table=""
 data = `nvidia-smi --query-gpu=gpu_name,temperature.gpu --format=csv,noheader`
 arr = data.split("\n")
-table = "<table>"
 arr.each do |card|
-  table << "<tr><td>"
   card_data = card.split(",")
-  table << "#{card_data[0]} - #{card_data[1]}C"
-  table << "</td></tr>"
+  table << "#{card_data[0]} - #{card_data[1]}C\n"
 end
-table << "</table>"
 p table
